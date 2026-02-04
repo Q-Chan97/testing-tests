d00e034f5a4f125c27479085df86f067966df1db
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-03 22:43:48 -0600

    feat: Write tests for myCalculator

--- src/fns.test.js
+++ src/fns.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./fns";
+import { capitalize, myCalculator, reverseString } from "./fns";
 
 test("Capitalize 'charlie'", () => {
     expect(capitalize("charlie")).toBe("Charlie");
@@ -6,4 +6,20 @@ test("Capitalize 'charlie'", () => {
 
 test("Reverse 'nilbog'", () => {
     expect(reverseString("nilbog")).toBe("goblin")
+})
+
+test("Add 2 + 2 to equal 4", () => {
+    expect(myCalculator.add(2, 2)).toEqual(4);
+})
+
+test("Subtract 2 - 2 to equal 0", () => {
+    expect(myCalculator.subtract(2, 2)).toEqual(0);
+})
+
+test("Multiply 2 * 3 to equal 6", () => {
+    expect(myCalculator.multiply(2, 3)).toEqual(6);
+})
+
+test("Divide 6 / 2 to equal 3", () => {
+    expect(myCalculator.divide(6, 2)).toEqual(3);
 })
