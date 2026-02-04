5f3dc657eba4ee35e9724d3021b996b373e63e6f
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-04 09:35:41 -0600

    feat: Implement caesarCipher encrypt helper

--- src/fns.js
+++ src/fns.js
@@ -23,3 +23,20 @@ export const myCalculator = {
         return a / b;
     }
 };
+
+export function caesarCipher(string, key) {
+    const alphabet = [
+        "a", "b", "c", "d", "e", "f", "g",
+        "h", "i", "j", "k", "l", "m", "n",
+        "o", "p", "q", "r", "s", "t", "u",
+        "v", "w", "x", "y", "z"
+    ]
+
+    function encrypt(char) {
+        const position = alphabet.indexOf(char); // Gets index of letter
+        const newPosition = (position + key) % 26; // Transforms index based on key
+        return alphabet[newPosition]; // Returns new letter based on new index position
+    }
+
+    return string.split("").map(encrypt).join(""); // Splits string, encrypts each character, and joins
+}
