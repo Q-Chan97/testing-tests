6b703558399a5c0db5462b8ad75ffae999c59d55
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-03 21:50:00 -0600

    feat: Implement reverseString function

--- src/fns.js
+++ src/fns.js
@@ -1,3 +1,7 @@
 export function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function reverseString(string) {
+    return string.split("").reverse().join(""); // Splits string into each character, reverses it, and joins it together again
+}
