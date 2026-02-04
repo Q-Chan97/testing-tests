2811e104ec362c3c4541673495df5527374c58b7
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-03 23:13:35 -0600

    feat: Implement myCalculator

--- src/fns.js
+++ src/fns.js
@@ -5,3 +5,21 @@ export function capitalize(string) {
 export function reverseString(string) {
     return string.split("").reverse().join(""); // Splits string into each character, reverses it, and joins it together again
 }
+
+export const myCalculator = {
+    add(a, b) {
+        return a + b;
+    },
+
+    subtract(a, b) {
+        return a - b;
+    },
+
+    multiply(a, b) {
+        return a * b;
+    },
+
+    divide(a, b) {
+        return a / b;
+    }
+};
