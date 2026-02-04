4dc1295fe4dc91fd0558a9d46c3d6b6ac6e003b6
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-03 21:33:06 -0600

    feat: Write test for reverseString

--- src/fns.test.js
+++ src/fns.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./fns";
 test("Capitalize 'charlie'", () => {
     expect(capitalize("charlie")).toBe("Charlie");
 })
+
+test("Reverse 'Nilbog'", () => {
+    expect(reverseString("nilbog")).toBe("goblin")
+})
